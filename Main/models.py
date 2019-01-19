912b6e832859bfa378026f5d0751565da0b954f2
Michael Hardy <98.michael.hardy@gmail.com>
2019-01-19 10:49:21 -0800

    Adding book

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book():
+	isbn = models.CharField(max_length=16)
+	name = models.CharField(max_length=512)
+	author = models.CharField(max_length=256)
+	class = models.CharField(max_length=128)
+	publisher = models.CharField(max_length=512)
+	hardcover = models.NullBooleanField()
+	version = models.CharField(max_length=128)
+	uuid = models.CharField(max_length=16)
