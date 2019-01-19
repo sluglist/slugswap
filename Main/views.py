1a7e35ae1676b1899ab075a8799cda4f7d77669b
Michael Hardy <98.michael.hardy@gmail.com>
2019-01-18 22:32:58 -0800

    Adding basic url handling

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Home')
+
+def login(request):
+    return HttpResponse('Login')
+
+def items(request, direction, catagory=None):
+    return HttpResponse('Items')
+
+def item(request, id):
+    return HttpResponse('item')
+
+def inbox(request):
+    return HttpResponse('inbox')
