9533b19010c014c5ec7b4ed0b5975b1866c836bd
Michael Hardy <98.michael.hardy@gmail.com>
2019-01-19 15:14:53 -0800

    Bugfixes and schema update

--- Main/models.py
+++ Main/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Book():
+class Book(models.Model):
 	isbn = models.CharField(max_length=16)
 	name = models.CharField(max_length=512)
 	author = models.CharField(max_length=256)
-	class = models.CharField(max_length=128)
+	course = models.CharField(max_length=128)
 	publisher = models.CharField(max_length=512)
 	hardcover = models.NullBooleanField()
+	want = models.BooleanField()
 	version = models.CharField(max_length=128)
 	id = models.CharField(max_length=16, primary_key=True)
 	sold = models.BooleanField()
 	poster = models.EmailField()
 
-class Comment():
+class Comment(models.Model):
 	id = models.CharField(max_length=16, primary_key=True)
 	target = models.ForeignKey(Book, on_delete=models.CASCADE)
 	poster = models.EmailField()
