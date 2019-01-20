c29501171e7ea8fb0598002a65ff037c48023e0e
Michael Hardy <michardy@users.noreply.github.com>
2019-01-20 00:57:33 -0800

    Revert "This is why we don't forcemerge"

--- Main/views.py
+++ Main/views.py
@@ -4,12 +4,11 @@ from django.http import HttpResponse
 from Main.models import Book
 import uuid
 
+
 def index(request):
-    user_wants = Book.objects.filter(want=True).all()
-    user_has = Book.objects.filter(want=False).all()
     context = {
-        'user_wants': user_wants,
-        'user_has': user_has,
+        'user_wants': [],
+        'user_has': [],
         'others_want': [],
         'others_have': [],
     }
